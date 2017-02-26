8f1499b0404a4e0499e243f2e33ee6150aec2d40
Paritosh Mishra <paritosh149@outlook.com>
2017-02-26 10:43:04 +0530

    Hero Master details completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from './models/hero';
+import { HEROES } from './mocks/hero-mock';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app/app.component.html',
+  styleUrls: ['./app/app.component.css']
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  title = 'Tour of Heroes';
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero;
+
+  selectHero(hero: Hero) {
+    this.selectedHero = hero;
+  }
+}
